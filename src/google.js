7d7e90d7e56565217a2a6d5001f7b6659a6a64e8
Miandritiana <miandrititana21@gmail.com>
2024-02-28 18:19:19 +0300

    fllux

--- src/google.js
+++ src/google.js
@@ -3,5 +3,5 @@ export const initializeGoogleAnalytics = () => {
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', 'G-954JMH7ZY4');
+    gtag('config', 'G-0EKT2MP3QP');
 };
